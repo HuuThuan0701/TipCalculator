044d46536bb7a1fc3b24b98f1dd36980004392af
Thuan Nguyen <huuthuan0701@gmail.com>
2015-11-02 14:58:10 +0700

    Add some features

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -13,13 +13,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    @IBOutlet weak var peopleLabel: UILabel!
+    @IBOutlet weak var peopleShare: UILabel!
+    @IBOutlet weak var peopleStepper: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
+        tipLabel.text = "$0.0"
+        totalLabel.text = "$0.0"
+        tipPercentageLabel.text = "10%"
+        billField.becomeFirstResponder()
+        peopleShare.text = totalLabel.text
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,22 +35,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.1, 0.15, 0.2]
-        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let tipPercentage = Int(round(tipPercentageSlider.value))
         
         var billAmount = Double(billField.text!)
         if billAmount == nil {
             billAmount = 0
         }
-        let tip = Double(billAmount! * tipPercentage)
+        
+        let tip = billAmount! * Double(tipPercentage) / 100
         let total = tip + billAmount!
+        
+        let people = Int(peopleStepper.value)
+        let peopleEach = round(total / Double(people) * 100) / 100
         
         tipLabel.text = "$\(tip)"
         totalLabel.text = "$\(total)"
-    }
-    
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
+        tipPercentageLabel.text = "\(tipPercentage)%"
+        peopleLabel.text = "\(people) People"
+        peopleShare.text = "$\(peopleEach)"
+        
     }
 }
 
